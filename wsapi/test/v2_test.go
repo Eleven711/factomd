003962d25f7e4f50f1f2e2187b1ba912137fb76a
Matthew Whittington <matt@mattwhittington.com>
2016-05-30 14:16:58 -0500

    map to Struct test file

---
+++ wsapi/test/v2_test.go
@@ -0,0 +1,37 @@
+package wsapi
+
+import (
+	"fmt"
+	"testing"
+	"github.com/FactomProject/factomd/common/primitives"
+)
+
+type AddressRequest struct {
+	Address string `json:"address"`
+}
+
+func TestHandleFactoidBalance(t *testing.T) {
+
+	jsonStr := []byte(`{"jsonrpc":"2.0","id":0,"params":{"Address":"FA2jK2HcLnRdS94dEcU27rF3meoJfpUcZPSinpb7AwQvPRY6RL1Q"},"method":"factoid-balance"}`)
+
+	j, err := primitives.ParseJSON2Request(string(jsonStr))
+	params := j.Params
+	
+	if err != nil {
+		t.Log("%s",err)
+	}
+
+	t.Log("params: %s",params)
+
+	HandleV2FactoidBalance(params,t)	
+}
+
+func HandleV2FactoidBalance(params interface{},t *testing.T)  {
+	t.Log("params:",params) 
+	
+	fadr, ok := params.(AddressRequest)
+     	if !ok {
+		t.Errorf("ok: %b",ok)
+	}
+	fmt.Println(fadr)	
+}
